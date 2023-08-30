e80d72dda8ee458e25796f465a74e44dfad5306b
Carlos Cabrera <cecabrera@applaudostudios.com>
2023-08-30 17:37:00 -0600

    :sparkles: Add basic Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav className="h-14 w-full bg-gray-50 p-3 flex justify-between">
+      <div>Photo</div>
+      <div>Options</div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,5 +1,11 @@
+import Navbar from '../Navbar/Navbar';
+
 const SideBar = () => {
-  return <div className="w-2/5  bg-gray-75 text-white">SideBar</div>;
+  return (
+    <div className="w-2/5  bg-gray-75 text-white">
+      <Navbar />
+    </div>
+  );
 };
 
 export default SideBar;
