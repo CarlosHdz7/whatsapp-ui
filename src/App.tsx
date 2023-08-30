98a7509ad7aa185af125e84d236fa45c3159d4cf
Carlos Cabrera <cecabrera@applaudostudios.com>
2023-08-30 17:08:11 -0600

    :sparkles: Add MainContent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import MainContent from './components/MainContent/MainContent';
 import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
     <div className="flex">
       <SideBar />
+      <MainContent />
     </div>
   );
 }
---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainContent = () => {
+  return <div className="bg-gray-300 pl-72 w-full">MainContent</div>;
+};
+
+export default MainContent;
