929400307214a58a07ded4773f2f924f57357d23
Carlos Cabrera <cecabrera@applaudostudios.com>
2023-08-30 17:44:29 -0600

    :sparkles: Add icons to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,22 @@
+import {
+  BsFillPeopleFill,
+  BsFillChatLeftTextFill,
+  BsThreeDotsVertical,
+} from 'react-icons/bs';
+import { FaHistory } from 'react-icons/fa';
+
 const Navbar = () => {
   return (
     <nav className="h-14 w-full bg-gray-50 p-3 flex justify-between">
-      <div>Photo</div>
-      <div>Options</div>
+      <div className="flex justify-center items-center">
+        <div className="rounded-3xl bg-gray-1 h-8 w-8" />
+      </div>
+      <div className="flex items-center gap-8">
+        <BsFillPeopleFill size={20} />
+        <FaHistory size={20} />
+        <BsFillChatLeftTextFill size={20} />
+        <BsThreeDotsVertical size={20} />
+      </div>
     </nav>
   );
 };
