38cda23b7294b21750f8c9c999ccb9177c7cc4f7
Carlos Cabrera <cecabrera@applaudostudios.com>
2023-08-30 17:12:11 -0600

    :lipstick: Add color palette to tailwind config

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const MainContent = () => {
   return <div className="bg-gray-300 pl-72 w-full">MainContent</div>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.tml', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00A884',
+        gray: {
+          100: '#0C1317',
+          75: '#111B21',
+          50: '#202C33',
+          25: '#222E35',
+          1: '#AEBAC1',
+        },
+      },
+    },
   },
   plugins: [],
 };
