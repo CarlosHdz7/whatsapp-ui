fe19f39b36e8a1528cf2c0f9fa7c7792a2f1452d
Carlos Cabrera <cecabrera@applaudostudios.com>
2023-08-30 22:18:25 -0600

    :clown_face: Add mocked data for contacts

--- src/components/Chats/Chats.tsx
+++ src/components/Chats/Chats.tsx
@@ -2,10 +2,7 @@ import Chat from './Chat/Chat';
 
 const Chats = () => {
   return (
-    <section
-      className="flex gap-y-2 flex-col pl-3 pt-3 pr-3 overflow-y-scroll w-full
-    "
-    >
+    <section className="flex gap-y-2 flex-col pl-3 pt-3 pr-3 overflow-y-scroll w-full">
       <Chat />
       <Chat />
       <Chat />
---
+++ src/data/contacts.ts
@@ -0,0 +1,188 @@
+type ContactsType = {
+  _id: string;
+  index: number;
+  name: string;
+  status?: string;
+  picture: string;
+  lastConexion: string;
+  message: string;
+  numberOfMessage: number;
+  statusMessage: string;
+};
+
+export const contacts: ContactsType[] = [
+  {
+    _id: '64f0140238180729eaed34ec',
+    index: 0,
+    name: 'Martina',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '12:06 P.M',
+    message:
+      'nostrud ullamco cupidatat tempor nisi consequat laborum sit nisi exercitation',
+    numberOfMessage: 3,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f0140248e94ad5d1b55f7e',
+    index: 1,
+    name: 'Payne',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '03:28 A.M',
+    message:
+      'officia reprehenderit sunt elit proident pariatur occaecat sit in enim',
+    numberOfMessage: 3,
+    statusMessage: 'sent',
+  },
+  {
+    _id: '64f01402b542819f4a9517db',
+    index: 2,
+    name: 'Baker',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '01:15 A.M',
+    message: 'laborum laborum veniam ullamco consectetur mollit ea do eu et',
+    numberOfMessage: 2,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f01402a31e0d7687f26d26',
+    index: 3,
+    name: 'Ada',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '07:28 A.M',
+    message:
+      'duis amet reprehenderit commodo voluptate anim ipsum culpa esse consequat',
+    numberOfMessage: 7,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f0140295655cc8dc2e2052',
+    index: 4,
+    name: 'Selma',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '05:05 P.M',
+    message:
+      'cillum ea ea cupidatat culpa consequat adipisicing adipisicing pariatur veniam',
+    numberOfMessage: 3,
+    statusMessage: 'sent',
+  },
+  {
+    _id: '64f01402c43ca4b0bfea580d',
+    index: 5,
+    name: 'Hodge',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '10:35 A.M',
+    message: 'aliqua in qui do velit proident laborum sint ex aliquip',
+    numberOfMessage: 2,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f0140223b512e70213e384',
+    index: 6,
+    name: 'Constance',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '06:21 P.M',
+    message: 'officia dolor ea do amet enim irure sunt commodo ex',
+    numberOfMessage: 7,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f01402eba1d51ec9f0c700',
+    index: 7,
+    name: 'Branch',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '10:14 P.M',
+    message:
+      'laboris exercitation laboris aliqua est exercitation enim in pariatur nostrud',
+    numberOfMessage: 2,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f0140245f8f9a20de27b75',
+    index: 8,
+    name: 'Abby',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '02:07 A.M',
+    message:
+      'sunt exercitation anim proident proident reprehenderit minim consequat do consequat',
+    numberOfMessage: 0,
+    statusMessage: 'sent',
+  },
+  {
+    _id: '64f01402424d6b3af92e1cc2',
+    index: 9,
+    name: 'Cantrell',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '04:55 P.M',
+    message: 'sunt ea incididunt qui sint dolor ex eu officia ullamco',
+    numberOfMessage: 7,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f01402b17ff17dd3d863f9',
+    index: 10,
+    name: 'Flores',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '03:17 P.M',
+    message:
+      'cupidatat consequat nisi exercitation nisi occaecat officia veniam occaecat ad',
+    numberOfMessage: 3,
+    statusMessage: 'sent',
+  },
+  {
+    _id: '64f01402971a6a6d67de9b33',
+    index: 11,
+    name: 'Jackson',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '11:25 P.M',
+    message:
+      'ut excepteur aute do reprehenderit id irure labore exercitation amet',
+    numberOfMessage: 3,
+    statusMessage: 'received',
+  },
+  {
+    _id: '64f014022bca3b0b4731f94c',
+    index: 12,
+    name: 'Annmarie',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '03:56 P.M',
+    message: 'commodo pariatur ullamco do labore magna minim irure fugiat quis',
+    numberOfMessage: 3,
+    statusMessage: 'sent',
+  },
+  {
+    _id: '64f014027a704c0546499863',
+    index: 13,
+    name: 'Latonya',
+    status: 'undefined',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '09:27 P.M',
+    message:
+      'quis irure ullamco magna nisi et ad voluptate eiusmod adipisicing',
+    numberOfMessage: 4,
+    statusMessage: 'sent',
+  },
+  {
+    _id: '64f0140251f5eba7afecfda7',
+    index: 14,
+    name: 'Sims',
+    status: 'Online',
+    picture: 'http://placehold.it/32x32',
+    lastConexion: '01:54 A.M',
+    message: 'magna amet ea ad cupidatat consectetur minim nostrud ea ut',
+    numberOfMessage: 1,
+    statusMessage: 'sent',
+  },
+];
